a872883f251f49ae36eae43b6d1c633757361071
Kate <kate.nordqvist@gmail.com>
2023-12-14 21:24:44 +0100

    LoginContainer and styled

--- src/components/Login/LoginContainer.styled.tsx
+++ src/components/Login/LoginContainer.styled.tsx
@@ -3,9 +3,10 @@ import styled from 'styled-components';
 export const StyledLoginContainer = styled.div`
 	display: flex;
 
-	border: 2px solid red;
+	border: 2px solid var(--light-grey);
 	height: 40vh;
 	width: 40rem;
+	background-color: #ffffff;
 	justify-content: space-evenly;
 	align-items: center;
 `;
--- src/components/Login/LoginContainer.tsx
+++ src/components/Login/LoginContainer.tsx
@@ -1,11 +1,26 @@
+import { FormEvent } from 'react';
+import CustomButton from '../Buttons/CustomButton';
 import TextInput from '../Inputs/CustomTextInput';
 import { StyledLoginContainer } from './LoginContainer.styled';
 
 export const LoginContainer = () => {
+	const handleLogin = (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		const target = e.currentTarget.elements;
+
+		const username = target.namedItem('Username') as HTMLInputElement | null;
+		const password = target.namedItem('Password') as HTMLInputElement | null;
+
+		console.log(username?.value, password?.value);
+	};
+
 	return (
 		<StyledLoginContainer>
-			<TextInput type='text' name='Username' />
-			<TextInput type='password' name='Password' />
+			<form onSubmit={handleLogin}>
+				<TextInput type='text' name='Username' />
+				<TextInput type='password' name='Password' />
+				<CustomButton className='large' text='Log in' type='submit' />
+			</form>
 		</StyledLoginContainer>
 	);
 };
