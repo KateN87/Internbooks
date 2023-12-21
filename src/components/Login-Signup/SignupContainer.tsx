7f091a9487de1708838cc2957f1e3d731c3133e0
Kate <kate.nordqvist@gmail.com>
2023-12-21 21:54:29 +0100

    make style work with customizable nr of inputs

--- src/components/Login-Signup/SignupContainer.tsx
+++ src/components/Login-Signup/SignupContainer.tsx
@@ -1,90 +1,86 @@
 import { FormEvent, useState } from 'react';
 import CustomButton from '../Buttons/CustomButton';
-import { StyledLoginSignupContainer } from './Login-Signup.styled';
+import {
+  StyledInputContainer,
+  StyledLoginSignupContainer,
+} from './Login-Signup.styled';
 import { Link } from 'react-router-dom';
 import CustomInputContainer from '../CustomInput/CustomInputContainer';
+import signupParams from '../../params/signupParams';
 
 const SignupContainer = () => {
-	const [error, setError] = useState({ input: '', message: '' });
+  const [error, setError] = useState({ input: '', message: '' });
 
-	const handleSignup = (e: FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
-		const target = e.currentTarget.elements;
-		const username = target.namedItem('Username') as HTMLInputElement | null;
-		const password = target.namedItem('Password') as HTMLInputElement | null;
-		const repeatPassword = target.namedItem(
-			'Repeat Password'
-		) as HTMLInputElement | null;
+  const handleSignup = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const target = e.currentTarget.elements;
+    const username = target.namedItem('Username') as HTMLInputElement | null;
+    const password = target.namedItem('Password') as HTMLInputElement | null;
+    const repeatPassword = target.namedItem(
+      'Repeat Password'
+    ) as HTMLInputElement | null;
 
-		setError({
-			input: '',
-			message: '',
-		});
+    setError({
+      input: '',
+      message: '',
+    });
 
-		if (!username?.value) {
-			return setError({
-				input: 'username',
-				message: 'Please fill in username',
-			});
-		}
+    if (!username?.value) {
+      return setError({
+        input: 'username',
+        message: 'Please fill in username',
+      });
+    }
 
-		if (!password?.value) {
-			return setError({
-				input: 'password',
-				message: 'Please fill in password',
-			});
-		}
+    if (!password?.value) {
+      return setError({
+        input: 'password',
+        message: 'Please fill in password',
+      });
+    }
 
-		if (!repeatPassword?.value) {
-			return setError({
-				input: 'repeatPassword',
-				message: 'Please repeat password',
-			});
-		}
+    if (!repeatPassword?.value) {
+      return setError({
+        input: 'repeatPassword',
+        message: 'Please repeat password',
+      });
+    }
 
-		//handle login here
-		console.log(
-			'Username, password, repeatPassword: ',
-			username.value,
-			password.value,
-			repeatPassword.value
-		);
-	};
+    //handle login here
+    console.log(
+      'Username, password, repeatPassword: ',
+      username.value,
+      password.value,
+      repeatPassword.value
+    );
+  };
 
-	return (
-		<StyledLoginSignupContainer>
-			<h1>Sign up</h1>
+  return (
+    <StyledLoginSignupContainer>
+      <h1>Sign up</h1>
 
-			<form onSubmit={handleSignup}>
-				<CustomInputContainer
-					type='text'
-					name='Username'
-					error={error.input === 'username'}
-					errorMessage={error.message}
-				/>
+      <form onSubmit={handleSignup}>
+        <StyledInputContainer>
+          {signupParams.map(({ type, name, errorType }) => (
+            <CustomInputContainer
+              key={name}
+              type={type}
+              name={name}
+              error={error.input === errorType}
+              errorMessage={error.message}
+            />
+          ))}
+        </StyledInputContainer>
+        <div className="button-container">
+          <CustomButton className="large" text="Sign up" type="submit" />
+        </div>
+      </form>
 
-				<CustomInputContainer
-					type='password'
-					name='Password'
-					error={error.input === 'password'}
-					errorMessage={error.message}
-				/>
-
-				<CustomInputContainer
-					type='password'
-					name='Repeat Password'
-					error={error.input === 'repeatPassword'}
-					errorMessage={error.message}
-				/>
-
-				<CustomButton className='large' text='Sign up' type='submit' />
-			</form>
-
-			<Link to='/login'>
-				Already have an account? <b>Log in here.</b>
-			</Link>
-		</StyledLoginSignupContainer>
-	);
+      <Link to="/login">
+        Already have an account? <b>Log in here.</b>
+      </Link>
+    </StyledLoginSignupContainer>
+  );
 };
 
 export default SignupContainer;
---
+++ src/params/signupParams.tsx
@@ -0,0 +1,39 @@
+const signupParams = [
+  {
+    type: 'text',
+    name: 'Username',
+    errorType: 'username',
+  },
+  {
+    type: 'password',
+    name: 'Password',
+    errorType: 'password',
+  },
+  {
+    type: 'password',
+    name: 'Repeat Password',
+    errorType: 'repeatPassword',
+  },
+  {
+    type: 'tel',
+    name: 'Phone Number',
+    errorType: 'phoneNumber',
+  },
+  {
+    type: 'text',
+    name: 'Street',
+    errorType: 'street',
+  },
+  {
+    type: 'text',
+    name: 'Zip Code',
+    errorType: 'zip',
+  },
+  {
+    type: 'text',
+    name: 'City',
+    errorType: 'city',
+  },
+];
+
+export default signupParams;
