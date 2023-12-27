755691eeb94f5b4c86f2a8fa2f4d0993bd52e19e
Kate <kate.nordqvist@gmail.com>
2023-12-27 18:24:56 +0100

    fix navigate to book page
    set stop propagation if buy button is clicked

--- src/components/BookCard/BookCard.styled.tsx
+++ src/components/BookCard/BookCard.styled.tsx
@@ -1,45 +1,46 @@
 import styled from 'styled-components';
 
 export const StyledBookCard = styled.div`
-	display: flex;
-	border: 1px solid var(--medium-grey);
-	border-radius: 2px;
-	margin: 10px;
-	box-shadow: 1px 3px var(--light-grey);
-	width: 18rem;
-	height: 30vh;
-	justify-content: center;
-	align-items: center;
+  display: flex;
+  border: 1px solid var(--medium-grey);
+  border-radius: 2px;
+  margin: 10px;
+  box-shadow: 1px 3px var(--light-grey);
+  width: 18rem;
+  height: 30vh;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
 
-	& div {
-		width: 16rem;
-	}
-	& img {
-		width: 16rem;
-		height: 20vh;
-	}
+  & div {
+    width: 16rem;
+  }
+  & img {
+    width: 16rem;
+    height: 20vh;
+  }
 `;
 
 export const StyledBookInfo = styled.div`
-	position: relative;
-	height: 8vh;
+  position: relative;
+  height: 8vh;
 
-	.title {
-		font-size: 16px;
-	}
-	.author {
-		font-style: italic;
-		font-size: 10px;
-	}
-	.price {
-		font-size: 16px;
-	}
+  .title {
+    font-size: 16px;
+  }
+  .author {
+    font-style: italic;
+    font-size: 10px;
+  }
+  .price {
+    font-size: 16px;
+  }
 
-	.price-buy {
-		position: absolute;
-		right: 0;
-		bottom: 0;
-		width: fit-content;
-		text-align: right;
-	}
+  .price-buy {
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    width: fit-content;
+    text-align: right;
+  }
 `;
