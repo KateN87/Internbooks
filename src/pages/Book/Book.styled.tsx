9e388bd4627c88902e0ca3d5d0b0b09eff633c72
Kate <kate.nordqvist@gmail.com>
2023-12-27 21:05:48 +0100

    add function to book page + styling
    render book information:
    - title
    - author
    - genre
    - synopsis
    - styled the component

---
+++ src/pages/Book/Book.styled.tsx
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+
+export const BookMainContainer = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  height: 80vh;
+  padding: 10vh 1vh;
+  justify-content: space-evenly;
+`;
+
+export const BookImage = styled.div`
+  & img {
+    max-width: 60vw;
+    min-width: 20vh;
+    max-height: 50vh;
+  }
+`;
+
+export const BookInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  min-width: 20vh;
+  width: 40vw;
+  min-height: 50vh;
+
+  .genre {
+    font-style: italic;
+  }
+  .synopsis {
+    color: blue;
+  }
+`;
+
+export const ButtonContainer = styled.div`
+  align-self: end;
+`;
