e37d9f77661e2e19e8a18d0024fca83fb3a35216
Kate <kate.nordqvist@gmail.com>
2023-12-27 16:30:28 +0100

    update userContext
    add loginUser function
    addlogoutUser function

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -1,19 +1,29 @@
 import React, { createContext, useState, useMemo, useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 type UserContextType = {
   user: User | null;
+  loginUser: (userInfo: User) => void;
+  logoutUser: () => void;
 };
 
 export const UserContext = createContext<UserContextType>({
   user: null,
+  loginUser: () => {},
+  logoutUser: () => {},
 });
 
 export const UserProvider = ({ children }: { children: React.ReactNode }) => {
   const [user, setUser] = useState<User | null>(null);
+  const navigate = useNavigate();
 
-  const loginUser = useCallback((userInfo: User) => {
-    setUser(userInfo);
-  }, []);
+  const loginUser = useCallback(
+    (userInfo: User) => {
+      setUser(userInfo);
+      navigate('/');
+    },
+    [navigate]
+  );
 
   const logoutUser = useCallback(() => {
     setUser(null);
