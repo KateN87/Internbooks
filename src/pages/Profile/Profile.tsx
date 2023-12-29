2b5494339cb83c94b51cb97db6183987673fb4ca
Kate <kate.nordqvist@gmail.com>
2023-12-29 20:31:51 +0100

    Update Profile.tsx
    render users information

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,5 +1,24 @@
+import { useContext } from 'react';
+import { UserContext } from '../../context/UserContext';
+
 const Profile = () => {
-	return <div>Profile</div>;
+  const { user } = useContext(UserContext);
+
+  if (!user) {
+    return;
+  }
+  return (
+    <div>
+      <h2>Profile</h2>
+      <p>First Name: {user.firstname}</p>
+      <p>Last Name: {user.lastname}</p>
+      <p>Email: {user.email}</p>
+      <p>Address: {user.address}</p>
+      <p>City: {user.city}</p>
+      <p>Postcode: {user.postcode}</p>
+      <p>Phone Number: {user.phoneNumber}</p>
+    </div>
+  );
 };
 
 export default Profile;
