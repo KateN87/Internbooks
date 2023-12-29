c709ade7437da5a16abfd82d1da6c5ba6e091b3d
Kate <110845948+KateN87@users.noreply.github.com>
2023-12-29 21:00:53 +0100

    Automatically login user (#10)
    If no accesstoken, stay logged out.
    If accesstoken and user in localStorage, set user to localstorage info

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -5,7 +5,9 @@ import {
   useCallback,
   ReactNode,
   useContext,
+  useEffect,
 } from 'react';
+import Cookies from 'js-cookie';
 import { useNavigate } from 'react-router-dom';
 import Login from '../services/api/authApi';
 import { ErrorContext } from './ErrorContext';
@@ -27,14 +29,29 @@ export const UserProvider = ({ children }: { children: ReactNode }) => {
   const [user, setUser] = useState<User | null>(null);
   const navigate = useNavigate();
 
+  //automatically login user
+  useEffect(() => {
+    const maybeCookie = Cookies.get('accesstoken');
+    const maybeUser = localStorage.getItem('user');
+
+    if (!maybeCookie || !maybeUser) {
+      return;
+    }
+
+    return setUser(JSON.parse(maybeUser));
+  }, []);
+
   const loginUser = useCallback(
     async (formData: Record<string, string>) => {
       try {
         const userResponse: User = await Login(formData);
         const newUser = { ...userResponse };
         delete newUser.jwtToken;
-
         setUser(newUser);
+
+        const stringUser = JSON.stringify(newUser);
+        localStorage.setItem('user', stringUser);
+
         navigate('/');
       } catch (error) {
         handleError(error as CustomError);
