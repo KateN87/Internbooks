133e22b5338dccd58aed03ccd6e61c9347169e51
Kate <kate.nordqvist@gmail.com>
2024-01-12 21:20:10 +0100

    Create CombineBookArray.tsx

---
+++ src/Util/CombineBookArray.tsx
@@ -0,0 +1,22 @@
+const combinedArray: BookInventoryItem[] = [];
+
+// Function to combine books and inventory
+const combineBookArrays = (books: Book[], inventory: InventoryItem[]) => {
+  // Iterate through books
+  books.forEach((book) => {
+    // Find the corresponding inventory item
+    const inventoryItem = inventory.find(
+      (item) => item.itemCode === book.itemCode
+    );
+
+    // If inventory item exists, combine the information
+    if (inventoryItem) {
+      const combinedBook = { ...book, quantity: inventoryItem.quantity };
+      combinedArray.push(combinedBook);
+    }
+  });
+
+  return combinedArray;
+};
+
+export default combineBookArrays;
