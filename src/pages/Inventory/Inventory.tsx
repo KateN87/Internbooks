f08e438be9a16d33b8a9a7cb49566a9215c7f437
Kate <kate.nordqvist@gmail.com>
2024-01-12 21:20:07 +0100

    Update Inventory.tsx

--- src/pages/Inventory/Inventory.tsx
+++ src/pages/Inventory/Inventory.tsx
@@ -4,45 +4,27 @@ import { StyledInventory } from './Inventory.styled';
 import { useContext, useEffect } from 'react';
 import { BookContext } from '../../context/BookContext';
 import { InventoryContext } from '../../context/InventoryContext';
-
+import combineBookArrays from '../../Util/CombineBookArray';
+import MockBooks from '../../MockData/MockBooks.json';
+import BookEdit from './BookEdit/BookEdit';
 const Orders = () => {
   const [searchParams] = useSearchParams();
   const bookItem = searchParams.get('bookItem');
   const { bookList, getBooks } = useContext(BookContext);
   const { inventoryList, getInventories } = useContext(InventoryContext);
 
-  useEffect(() => {
+  /*   useEffect(() => {
     getBooks();
     getInventories();
   }, [getBooks, getInventories]);
 
-  const combinedArray: BookInventoryItem[] = [];
-
-  // Function to combine books and inventory
-  const combineArrays = (books: Book[], inventory: InventoryItem[]) => {
-    // Iterate through books
-    books.forEach((book) => {
-      // Find the corresponding inventory item
-      const inventoryItem = inventory.find(
-        (item) => item.itemCode === book.itemCode
-      );
-
-      // If inventory item exists, combine the information
-      if (inventoryItem) {
-        const combinedBook = { ...book, quantity: inventoryItem.quantity };
-        combinedArray.push(combinedBook);
-      }
-    });
-
-    return combinedArray;
-  };
-
   // Call the function and get the result
-  const resultArray = combineArrays(bookList, inventoryList);
+  const resultArray = combineBookArrays(bookList, inventoryList); */
 
   return (
     <StyledInventory className="side">
-      {!bookItem && <InventoryTable data={resultArray} />}
+      {!bookItem && <InventoryTable data={MockBooks} />}
+      {bookItem && <BookEdit bookItemCode={bookItem} />}
     </StyledInventory>
   );
 };
