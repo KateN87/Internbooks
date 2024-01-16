9cd900388e65a77dba9fc0f6ca993aa46293e2ea
Kate <kate.nordqvist@gmail.com>
2024-01-16 20:10:07 +0100

    show user info

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,5 +1,34 @@
+import { useContext } from 'react';
+import { UserContext } from '../../context/UserContext';
+import DataCard from '../../components/Cards/DataCard/DataCard';
+
 const Cart = () => {
-	return <div>Cart</div>;
+  const { user } = useContext(UserContext);
+
+  const userAddress = [
+    `${user?.firstname} ${user?.lastname}`,
+    user?.address,
+    `${user?.postcode} ${user?.city}`,
+  ];
+
+  const userInfo = {
+    Address: userAddress,
+  };
+
+  const paymentInfo = {
+    'Card number': '123-456-789',
+    'Expiration date': '11/24',
+    CVV: '123',
+  };
+
+  return (
+    <div>
+      <div>
+        <DataCard data={userInfo} />
+        <DataCard data={paymentInfo} />
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
