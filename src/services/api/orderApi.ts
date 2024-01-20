7eaa9a301a152aac74ff5de0604194f7a990eed6
Kate <kate.nordqvist@gmail.com>
2024-01-20 14:08:01 +0100

    Update orderApi.ts

--- src/services/api/orderApi.ts
+++ src/services/api/orderApi.ts
@@ -1,5 +1,5 @@
 import { AxiosError, isAxiosError } from 'axios';
-import { post } from '../httpService/http.methods';
+import { post, get } from '../httpService/http.methods';
 
 const baseUrl = import.meta.env.VITE_APP_BASE_URL;
 
@@ -17,10 +17,23 @@ export const postOrder = async (
           'Sorry, one or more items in your order is out of stock';
       }
     } else {
-      customError.message = 'Problem register.';
+      customError.message = 'Problem placing order. Please try again later.';
     }
     throw customError;
   }
 };
 
-/* Problem placing order in. Please try again later. */
+export const getUserOrder = async (
+  email: string
+): Promise<UserOrdersDataBase[]> => {
+  try {
+    return await get(`${baseUrl}order/${email}`, true);
+  } catch (error) {
+    console.error('getUserOrder:', error);
+    const customError: CustomError = {
+      message: 'Problem getting orders. Please try again later.',
+    };
+
+    throw customError;
+  }
+};
