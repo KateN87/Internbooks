3eea3eaf6383bc35e6b32bd9f6d4dadbaa3bdd86
Kate <kate.nordqvist@gmail.com>
2024-01-22 20:20:51 +0100

    calculateTotalPrice only 2 decimals

--- src/Util/calculateTotalPrice.tsx
+++ src/Util/calculateTotalPrice.tsx
@@ -1,15 +1,13 @@
-type CalculateProps = {
-  cart: CartItem[] | OrderItemDataBase[];
-};
-
-const calculateTotalPrice = ({ cart }: CalculateProps) => {
-  return cart.reduce((total, book) => {
+const calculateTotalPrice = (cart: CartItem[]) => {
+  const total = cart.reduce((total, book) => {
     // Calculate the price for each book by multiplying the quantity with the book price
     const bookPrice = (book.quantity || 1) * book.price;
 
     // Add the calculated price to the total
     return total + bookPrice;
   }, 0);
+  // round the total to 2 decimal places
+  return parseFloat(total.toFixed(2));
 };
 
 export default calculateTotalPrice;
