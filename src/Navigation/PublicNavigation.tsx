9801dc29e49e63a3a424dab860a8a7d6c92d5d44
Kate <kate.nordqvist@gmail.com>
2024-01-30 19:47:30 +0100

    add unauthorized page

--- src/Navigation/PublicNavigation.tsx
+++ src/Navigation/PublicNavigation.tsx
@@ -6,23 +6,27 @@ import Login from '../pages/LoginSignup/Login';
 import Signup from '../pages/LoginSignup/Signup';
 import NoRoute from '../pages/NoRoute';
 import LargeTemplate from '../pageTemplate/LargeTemplate';
+import Unauthorized from '../pages/Unauthorized/Unauthorized';
 
 const PublicNavigation = () => {
-	return (
-		<Routes>
-			<Route path='/' element={<GeneralTemplate />}>
-				<Route path='/' element={<Home />} />
-				<Route path='/book/:bookId' element={<Book />} />
-			</Route>
-			<Route path='/login' element={<LargeTemplate />}>
-				<Route path='/login' element={<Login />} />
-			</Route>
-			<Route path='/signup' element={<LargeTemplate />}>
-				<Route path='/signup' element={<Signup />} />
-			</Route>
-			<Route path='*' element={<NoRoute />} />
-		</Routes>
-	);
+  return (
+    <Routes>
+      <Route path="/" element={<GeneralTemplate />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/book/:bookId" element={<Book />} />
+      </Route>
+      <Route path="/login" element={<LargeTemplate />}>
+        <Route path="/login" element={<Login />} />
+      </Route>
+      <Route path="/signup" element={<LargeTemplate />}>
+        <Route path="/signup" element={<Signup />} />
+      </Route>
+      <Route path="/unauthorized" element={<GeneralTemplate />}>
+        <Route path="/unauthorized" element={<Unauthorized />} />
+      </Route>
+      <Route path="*" element={<NoRoute />} />
+    </Routes>
+  );
 };
 
 export default PublicNavigation;
