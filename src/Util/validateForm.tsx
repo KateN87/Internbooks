8ce2ea2a89d9143b62dde20b481ce74fc4eed9a2
Kate <kate.nordqvist@gmail.com>
2024-01-30 20:51:36 +0100

    add password validation

--- src/Util/validateForm.tsx
+++ src/Util/validateForm.tsx
@@ -7,11 +7,22 @@ const validateForm = ({
   target,
   params,
 }: ValidateFormProps): boolean | CustomError => {
+  const validPassword = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+
   for (const { name, errorType } of params) {
     const inputElement = target.namedItem(name) as HTMLInputElement | null;
 
     if (!inputElement?.value) {
       return { input: errorType, message: `Please fill in ${name}` };
+    }
+
+    if (name === 'password') {
+      if (!validPassword.test(inputElement?.value)) {
+        return {
+          input: errorType,
+          message: `Password does not meet the criteria`,
+        };
+      }
     }
   }
 
